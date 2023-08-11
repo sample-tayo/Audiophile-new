07114307483371c2dd36fc454af1ad93423d1dd0
sample-tayo <bolaji_makinde@ymail.com>
2023-08-11 18:37:26 +0100

    neyo

---
+++ src/components/GoBackBtn.jsx
@@ -0,0 +1,18 @@
+import { useNavigate } from "react-router-dom";
+import styles from "../styles/Goback.module.css";
+
+function GoBackBtn() {
+  const navigate = useNavigate();
+
+  const goBack = () => {
+    navigate(-1);
+  };
+
+  return (
+    <p onClick={goBack} className={styles.goBack}>
+      Go Back
+    </p>
+  );
+}
+
+export default GoBackBtn;
